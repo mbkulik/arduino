85e1f657e34305d889062050d6aee159e62fde74
Michael B. Kulik <mike.b.kulik@gmail.com>
2013-03-20 13:50:00 -0400

    removed old commented out code

--- blink/src/blink.cpp
+++ blink/src/blink.cpp
@@ -1,5 +1,4 @@
 #include <avr/io.h>
-//#include <util/delay.h>
 #include <JeeLib.h>
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
@@ -12,12 +11,10 @@ main( void ) {
 		PORTB = 0x20;
       
       	Sleepy::loseSomeTime(1000);
-		//_delay_ms( 5000 );
       
 		PORTB = 0x00;
       
       	Sleepy::loseSomeTime(1000);
-		//_delay_ms( 5000 );
 	}
 
   return 1;
